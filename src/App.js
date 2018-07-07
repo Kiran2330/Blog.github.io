eb5815e58e56c4b5e9c54014299e4b26e8715d9b
kiran2330 <mkjoshi30@gmail.com>
2018-07-07 19:01:21 +0530

    Initial setup of reactjs app

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="container-fluid">
+        <div className="row blog-container">
+          <div>
+            
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
